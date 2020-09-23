820f6f2156a6f3a991bfdbe93d4a8d13691936fe
wade881001 <71745580+wade881001@users.noreply.github.com>
2020-09-23 11:42:23 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+
+import Click from './src/Click';
+
+export default function App() {
+    const [count, setCount] = useState(10);
+
+    let countString = "count in App:" + count;
+
+    function updateCount(newCount) {
+
+        setCount(newCount);
+
+    }
+
+    useEffect(() => {
+
+        Alert.alert("count in App:" + count);
+    });
+  return (
+    <View style={styles.container}>
+          <Text>abc123</Text>
+          <Button title={countString} onPress={() => setCount(count + 1)} />
+
+          <Click count={count} update={updateCount} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Click.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { Alert, Button } from 'react-native';
+
+
+export default function Click(props) {
+    const [count, setCount] = useState(props.count);
+    let countString = "count:" + count;
+
+    function showCount() {
+
+        Alert.alert("count:" + count);
+
+        props.update(count);
+
+    }
+    useEffect(showCount);
+    return (
+        <Button title={countString} onPress={() => setCount(count + 1)} />
+
+
+
+    );
+
+
+}
